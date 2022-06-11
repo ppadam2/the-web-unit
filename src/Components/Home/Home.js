562457f6559271425a9ba497749b99d2c481e4c1
praneeshpadam1 <102962147+praneeshpadam1@users.noreply.github.com>
2022-06-11 01:41:28 -0600

    Added star wars style error message for mobile screen

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,14 +3,21 @@ import StandardCard from "../Card/Card.js";
 import "./Home.scss";
 
 const Home = () => {
+    const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+    
     return (
         <>
+            {isMobile && <h1 className="err">Please view the site on a desktop device. Mobile version hasn't been implemented yet.</h1>}
+            {!isMobile && 
+            <>
             <Header />
             <div className="cardList">
                 <StandardCard imageLink="/assets/images/LawDepot/LawDepot-Homepage.jpg" title="LawDepot" description="Online legal document service provider; Provide mechanism for user to create legal documents" isLink="false" goTo="/lawdepot" />
                 <StandardCard imageLink="/assets/images/Cycle.jpg" title="Cyclic Sequences" description="Paper exploring properties of cyclic sequences including Distances, Occurances and Prediction" isLink="true" goTo="/assets/papers/Cyclic-Sequences.pdf" />
                 <StandardCard imageLink="/assets/images/Algorithms.jpg" title="Algorithms" description="Select Algorithms, Data-Structures and Problems from the book Introduction to Algorithms" isLink="true" goTo="https://github.com/ppadam2/Algorithms" />
             </div>
+            </>
+            }
 
         </>
     )
